40b5e06bde3f2b10f46307d072a6ef60820e6f94
Eric Zou <cheetsheatoverlode@gmail.com>
2020-06-07 22:02:37 -0400

    Added more to the code, fixed queue, imported more dependencies in wallet.py, started implementing P2P network.

--- wallet.py
+++ wallet.py
@@ -1,20 +1,24 @@
 import hashlib
 import json
 import os
+import socket
+import datetime
 
 def getIp():
     ip = os.popen('curl -s ifconfig.me').readline()
     return ip
 
-def addTransaction(message, queue):
-    queue.digest(b"{0}".format(message))
-    return queue
-
-def createTransaction(startAddress, amount, fee, endAddress):
+def createTransaction(startAddress, amount, fee, endAddress, utcTime, queue):
+    with open("queue.json", "r") as outfile:
+        queue = json.loads(queue, outfile)
     transaction = {
                 "startAddress":str(startAddress),
                 "amount":float(amount),
                 "fee":float(fee),
-                "endAddress":str(endAddress)
+                "endAddress":str(endAddress),
+                "utcTime":datetime.datetime.utcnow()
                 }
+
+    with open("queue.json", "w") as outfile:
+        json.dump(queue, outfile)
     return transaction
