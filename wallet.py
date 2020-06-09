ea7e0616fbf726b3fe648a50d01ae503c876b9c8
Eric Zou <cheetsheatoverlode@gmail.com>
2020-06-08 22:29:58 -0400

    Added main loop, set up block sourcing on server side, worked out pseudocode for main loop.

--- wallet.py
+++ wallet.py
@@ -8,7 +8,7 @@ def getIp():
     ip = os.popen('curl -s ifconfig.me').readline()
     return ip
 
-def createTransaction(startAddress, amount, fee, endAddress, utcTime, queue):
+def createTransaction(startAddress, amount, fee, endAddress, utcTime):
     with open("queue.json", "r") as outfile:
         queue = json.loads(queue, outfile)
     transaction = {
@@ -16,9 +16,29 @@ def createTransaction(startAddress, amount, fee, endAddress, utcTime, queue):
                 "amount":float(amount),
                 "fee":float(fee),
                 "endAddress":str(endAddress),
-                "utcTime":datetime.datetime.utcnow()
+                "utcTime":datetime.datetime.utcnow(),
+                "difficulty":0,
+                "blockHeight":0
                 }
 
     with open("queue.json", "w") as outfile:
         json.dump(queue, outfile)
     return transaction
+
+def getBalance():
+    pass
+
+blockStatus = {
+    "upToDate":False,
+    "blockSource":False,
+    "lastBlock":0
+}
+
+#block source to other wallets
+source = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+source.bind((getIp(), 7777))
+
+#main loop
+while True:
+    print("Balance, options")
+    option = input("What would you like to do? (Enter the number) ")
